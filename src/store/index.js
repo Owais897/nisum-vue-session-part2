db39f1e3259e57ef8d55b1568676e85154a380c0
Muhammad Owais <m.owais2118@gmail.com>
2021-11-08 17:30:24 +0500

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,42 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    count: 0,
   },
   mutations: {
+    incCount(state) {
+      //payload
+      state.count++;
+    },
+    setCount(state, payload) {
+      state.count = payload;
+    },
   },
   actions: {
+    async addRandomNumber(context) {
+      let data = await fetch(
+        "https://www.random.org/integers/?num=1&min=-1000&max=1000&col=1&base=10&format=plain&rnd=new"
+      );
+      data = await data.json();
+      console.log("data: ", data);
+      context.commit("setCount", data);
+    },
   },
-  modules: {
-  }
-})
+  getters: {
+    squareState: (state, getters, rootState) => {
+      console.log("rootState: ", rootState);
+      console.log("getters: ", getters);
+      return state.count ** 2;
+    },
+    squareState2: (state, getters, rootState) => {
+      console.log("rootState: ", rootState);
+      console.log("getters: ", getters);
+      return state.count ** 3;
+    },
+  },
+  modules: {},
+});
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>{{ count }}</h2>
+    <button @click="incCount">increase</button>
   </div>
 </template>
+
+<script>
+import { mapState, mapMutations } from "vuex";
+export default {
+  name: "About",
+  props: {
+    msg: String,
+  },
+  computed: {
+    ...mapState(["count"]),
+  },
+  methods: {
+    ...mapMutations(["incCount"]),
+  },
+};
+</script>
